90d36218ecf9e77a1568c1eb14ea4c0f01061706
tarun <tarunkumar8059@gmail.com>
2023-09-12 02:24:48 -0700

    add validate

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,47 +1,51 @@
-
- "use-client"
+"use client"
 import Image from "next/image"
  import Link from "next/link"
  
-export default function Example() {
+ import { useForm } from "react-hook-form";
+
+ 
+export default function Login() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+
+
+
+
     return (
       <>
         
         <div className="bg-slate-900 flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Image
-            width={400} height={400}
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-white text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            
+            <h2 className="mt-10 text-white text-center text-2xl font-bold leading-9 tracking-tight ">
               Sign in to your account
             </h2>
           </div>
   
           <div className=" marker:mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" >
+            <form noValidate className="space-y-6" onSubmit={handleSubmit((data)=>{console.log(data)})}>
               <div>
-                <label  htmlFor="email" className="text-white block text-sm font-medium leading-6 text-gray-900">
+                <label  htmlFor="email" className="text-white block text-sm font-medium leading-6 ">
                   Email address
                 </label>
                 <div className="mt-2">
                   <input
                     id="email"
-                    name="email"
+                    {...register("email",{ required: "email is required",  pattern:{value:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,message:"please enter valid email"} })}
                     type="email"
                     autoComplete="email"
                     placeholder="email@example.com"
-                    required
+                    
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.email&&<p className=" text-red-700">{errors.email.message}</p>}
                 </div>
               </div>
   
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="text-white block text-sm font-medium leading-6 text-gray-900">
+                  <label htmlFor="password" className="text-white block text-sm font-medium leading-6 ">
                     Password
                   </label>
                   <div className="text-sm">
@@ -51,19 +55,20 @@ export default function Example() {
                   </div>
                 </div>
                 <div className="mt-2">
-                  <input
+                <input
                     id="password"
-                    name="password"
+                {...register("password",{required:"password is required",pattern:{value:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,message:`- at least 8 characters\n- must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number\n  - Can contain special characters`}})}
                     type="password"
                     autoComplete="current-password"
                     placeholder="password"
-                    required
+                
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.password&&<p className=" text-red-700">{errors.password.message}</p>}
                 </div>
               </div>
               <div className="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"  />
                          
                           
                             <span htmlFor="remember" className="text-gray-200 dark:text-gray-300 ml-3 text-sm" >Remember me</span>
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,81 +1,85 @@
 
- "use-client"
-import Image from "next/image"
+ "use client"
+
  import Link from "next/link"
+  
+ import { useForm } from "react-hook-form";
+
  
-export default function Example() {
+export default function SignUp() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+
     return (
       <>
         
         <div className="bg-slate-900 flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Image
-            width={400} height={400}
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-white text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign Up to your account
+           
+            <h2 className="mt-10 text-white text-center text-2xl font-bold leading-9 tracking-tight ">
+              Create Account
             </h2>
           </div>
   
           <div className=" marker:mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" >
+            <form noValidate className="space-y-6" onSubmit={handleSubmit((data)=>{console.log(data)})} >
               <div>
-                <label  htmlFor="name" className="text-white block text-sm font-medium leading-6 text-gray-900">
+                <label  htmlFor="name" className="text-white block text-sm font-medium leading-6 ">
                   Name
                 </label>
                 <div className="mt-2">
                   <input
                     id="name"
-                    name="name"
+                 {...register("name",{required:"please enter name",minLength:{value:3,message:"please enter at least 3 letters"}})}
                     type="text"
                     autoComplete="name"
                     placeholder="Enter Your Name"
-                    required
+                 
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.name&&<p className=" text-red-700">{errors.name.message}</p>}
                 </div>
               </div>
               <div>
-                <label  htmlFor="email" className="text-white block text-sm font-medium leading-6 text-gray-900">
+                <label  htmlFor="email" className="text-white block text-sm font-medium leading-6 ">
                   Email address
                 </label>
                 <div className="mt-2">
-                  <input
+                <input
                     id="email"
-                    name="email"
+                    {...register("email",{ required: "email is required",  pattern:{value:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,message:"please enter valid email"} })}
                     type="email"
                     autoComplete="email"
                     placeholder="email@example.com"
-                    required
+                    
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.email&&<p className=" text-red-700">{errors.email.message}</p>}
                 </div>
               </div>
   
               <div>
-                <label  htmlFor="Mobileno" className="text-white block text-sm font-medium leading-6 text-gray-900">
+                <label  htmlFor="Mobileno" className="text-white block text-sm font-medium leading-6 ">
                   Mobile No:
                 </label>
                 <div className="mt-2">
                   <input
                     id="number"
-                    name="number"
-                    type="number"
-                    autoComplete="number"
+                    
+                  
+                    
                     placeholder="Enter Your Mobile No:"
-                    required
+                    {...register("number",{required:"Mobile No is required ",minLength:{value:10,message:"please enter a valid mobile number"},maxLength:{value:10,message:"please enter a valid mobile number"}})}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                   {errors.number&&<p className=" text-red-700">{errors.number.message}</p>}
                 </div>
               </div>
               
   
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="text-white block text-sm font-medium leading-6 text-gray-900">
+                  <label htmlFor="password" className="text-white block text-sm font-medium leading-6 ">
                     Password
                   </label>
                  
@@ -83,13 +87,36 @@ export default function Example() {
                 <div className="mt-2">
                   <input
                     id="password"
-                    name="password"
+                {...register("password",{required:"password is required",pattern:{value:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,message:`- at least 8 characters
+                - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
+                - Can contain special characters`}})}
                     type="password"
                     autoComplete="current-password"
                     placeholder="password"
-                    required
+                
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.password&&<p className=" text-red-700">{errors.password.message}</p>}
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="text-white block text-sm font-medium leading-6 ">
+                   Confirm Password
+                  </label>
+                 
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="confirm_password"
+                 {...register("confirm_password",{validate: (value, formValues) => value === formValues.password||"pasword not mach"})}
+                    type="password"
+                    autoComplete="current-password"
+                    placeholder="confirm password"
+                
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                  {errors.confirm_password&&<p className=" text-red-700">{errors.confirm_password.message}</p>}
                 </div>
               </div>
              
@@ -102,7 +129,13 @@ export default function Example() {
                 </button>
               </div>
             </form>
-  
+            <p className="mt-10 text-center text-sm  text-gray-200">
+              
+              If Account has already
+              <Link href="/login" className="font-semibold leading-6 text-indigo-400 hover:text-indigo-500 ml-3">
+                Sign In
+              </Link>
+            </p>
           
           </div>
         </div>
