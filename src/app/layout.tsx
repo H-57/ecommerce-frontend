94cf045987b86671d7cc30508fef03b541be60bc
tarun <tarunkumar8059@gmail.com>
2023-09-19 02:29:27 -0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from'../components/navbar/Navbar'
 import Footer from'../components/footer/Footer'
 import ToastContainer  from '../components/toast/toastContainer'
 import ReduxProvider from "../store/provider"
-import Parent from "../components/main/parent"
+import Parent from "../components/main/Parent"
 
 export const metadata = {
   title: 'Ecommerce app in next js',
